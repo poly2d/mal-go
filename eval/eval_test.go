b2b47f4d6a40feecda6b69c250abb27035745cfa
poly2d <43548622+poly2d@users.noreply.github.com>
2019-10-27 14:31:59 -0700

    🚨 s2 | eval: add test for EvalAst

---
+++ eval/eval_test.go
@@ -0,0 +1,32 @@
+package eval
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/poly2d/mal-go/env"
+	"github.com/poly2d/mal-go/read"
+)
+
+func TestReadStr(t *testing.T) {
+	var tests = []struct {
+		in       string
+		expected string
+	}{
+		{"abc", "abc"},
+		{"123   ", "123"},
+		{"(+ 2  3)", "5"},
+		{"     (+ 2 (* 3   4 )  )", "14"},
+
+		// Todo: figure out handling of eval of list without symbol.
+		{"( 123 456 789 )", "(123 456 789)"},
+	}
+
+	// Todo: check for panics on ill-formed inputs
+	for _, test := range tests {
+		ast := read.ReadStr(test.in)
+		actual := EvalAst(ast, env.ReplEnv)
+		assert.Equal(t, test.expected, actual.Sprint())
+	}
+}

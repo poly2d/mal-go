f24bf17642e9bef32dee84ba655f53beb240c0e9
poly2d <43548622+poly2d@users.noreply.github.com>
2019-10-26 23:09:44 -0700

    🚨 s1 | read: add test for ReadStr

---
+++ read/read_test.go
@@ -0,0 +1,24 @@
+package read
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadStr(t *testing.T) {
+	var tests = []struct {
+		in       string
+		expected string
+	}{
+		{"abc", "abc"},
+		{"123   ", "123"},
+		{"( 123 456 789 )", "(123 456 789)"},
+		{"     (+ 2 (* 3   4 )  )", "(+ 2 (* 3 4))"},
+	}
+
+	for _, test := range tests {
+		mf := ReadStr(test.in)
+		assert.Equal(t, test.expected, mf.Sprint())
+	}
+}

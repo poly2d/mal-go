250aa403df85e0469b264db624b4c13645766422
poly2d <43548622+poly2d@users.noreply.github.com>
2019-10-27 14:02:48 -0700

    🚧 s2 | eval: apply MalFunc from env if symbol found

--- eval/eval.go
+++ eval/eval.go
@@ -6,6 +6,33 @@ import (
 	"github.com/poly2d/mal-go/model"
 )
 
+func getFunc(mf model.MalForm, env model.MalEnv) model.MalFunc {
+	if mf.Type != model.MalTypeSymbol {
+		panic(fmt.Sprintf("getFunc was called on a non-symbol MalForm mf=%v", mf))
+	}
+
+	sym := mf.Value.(string)
+	f, exist := env[sym]
+	if !exist {
+		panic(fmt.Sprintf("Symbol %s does not exist in env", sym))
+	}
+
+	return f
+}
+
+func evalList(list []model.MalForm, env model.MalEnv) model.MalForm {
+	lead := list[0]
+	if lead.Type != model.MalTypeSymbol {
+		return model.MalForm{
+			model.MalTypeList,
+			list,
+		}
+	}
+
+	malFunc := getFunc(lead, env)
+	return malFunc(list[1:])
+}
+
 func EvalAst(ast model.MalForm, env model.MalEnv) model.MalForm {
 	switch ast.Type {
 	case model.MalTypeList:
@@ -18,20 +45,7 @@ func EvalAst(ast model.MalForm, env model.MalEnv) model.MalForm {
 		for _, member := range list {
 			newList = append(newList, EvalAst(member, env))
 		}
-		return model.MalForm{
-			model.MalTypeList,
-			newList,
-		}
-
-	case model.MalTypeSymbol:
-		sym := ast.Value.(string)
-
-		if _, exist := env[sym]; !exist {
-			panic(fmt.Sprintf("Symbol %s does not exist in env", sym))
-		}
-
-		// Todo: apply the func from env.
-		return ast
+		return evalList(newList, env)
 	}
 	return ast
 }

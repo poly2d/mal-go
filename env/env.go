34c52028781b202f9cc78ac6130144ce6476b678
poly2d <43548622+poly2d@users.noreply.github.com>
2019-10-27 00:14:02 -0700

    🚧 s2 | add initial simple repl env

---
+++ env/env.go
@@ -0,0 +1,8 @@
+package env
+
+var ReplEnv = map[string]func(int, int) int{
+	"+": func(a, b int) int { return a + b },
+	"-": func(a, b int) int { return a - b },
+	"*": func(a, b int) int { return a * b },
+	"/": func(a, b int) int { return int(a / b) },
+}
